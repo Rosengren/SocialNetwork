cc5f37f92454c739895eec2b294edcc2410a141c
Rosengren <krosengr@gmail.com>
2014-07-13 17:25:15 -0400

    added tests for status updates

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -23,7 +23,15 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create status" do
+  test "should be logged in to post a status" do
+    post :create, status: { content: "Hello" }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when logged in" do
+    sign_in users(:kevin)
+
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -31,13 +39,20 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to status_path(assigns(:status))
   end
 
-  test "should show status" do
-    get :show, id: @status
+  test "should get edit when logged in" do
+    sign_in users(:kevin)
+    get :edit, id: @status
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, id: @status
+  test "should redirect status update when not logged in" do
+    put :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should show status" do
+    get :show, id: @status
     assert_response :success
   end
 
